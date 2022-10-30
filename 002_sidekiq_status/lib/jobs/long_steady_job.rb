00fc67ea941160e96ab86331d9d403ed03df8f97
Michael Milewski <saramic@gmail.com>
2022-10-31 06:18:06 +1100

    Get SidekiqStatus working to display polling

--- 002_sidekiq_status/lib/jobs/long_steady_job.rb
+++ 002_sidekiq_status/lib/jobs/long_steady_job.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
 require "sidekiq"
+require "sidekiq-status"
+$LOAD_PATH << File.join(__dir__, "../../")
+require "config/environment"
 
 module Jobs
   class LongSteadyJob
-    include Sidekiq::Job
+    include Sidekiq::Worker
+    include Sidekiq::Status::Worker
 
     def perform(duration = 60)
       @start = Time.now
-      while (Time.now.to_i - @start.to_i) < duration
+      while (Time.now.to_f - @start.to_i) < duration
         sleep 1
+        percentage = ((Time.now.to_f - @start.to_i) / duration) * 100
+        percentage = 100 if percentage > 100.0
+        at percentage
       end
     end
   end
--- 002_sidekiq_status/sidekiq_web_config.ru
+++ 002_sidekiq_status/sidekiq_web_config.ru
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require "sidekiq/web"
-require "rack/session"
+require "sidekiq-status/web"
+$LOAD_PATH << __dir__
+require "config/environment.rb"
 
 use Rack::Session::Cookie,
   secret: File.read(".session.key"),
