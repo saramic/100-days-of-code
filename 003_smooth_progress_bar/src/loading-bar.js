daaf41c11a229eb55d502c18c4c0b46be2c7339e
Michael Milewski <saramic@gmail.com>
2022-10-31 19:56:19 +1100

    Day 003 a smooth Javascript animated progress bar
    that is just animating smoothly and NOT yet connected to any backend
    logic

--- 003_smooth_progress_bar/src/loading-bar.js
+++ 003_smooth_progress_bar/src/loading-bar.js
@@ -1,14 +1,49 @@
 //@ts-check
-const main = async () => {
-  const loadingBar = document.getElementById("loading-bar");
-  const startLoadingButton = document.getElementById("start-loading");
 
-  startLoadingButton.disabled = false;
+/** @param {HTMLDivElement} myBar */
+const progress = (myBar) => {
+  const id = setInterval(frame, 10);
+  function frame() {
+    const widthStr = myBar.style.width || "0%"
+    const width = parseFloat(widthStr.slice(0, -1), 10)
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      myBar.style.width = width + 0.05 + "%";
+    }
+  }
+}
 
-  startLoadingButton.onclick = () => {
-    startLoadingButton.disabled = true;
-    loadingBar.innerText = `loading ...`;
-  };
+/**
+ * @param {HTMLDivElement} progressBarContainer
+ * @returns {HTMLDivElement}
+ */
+const setupProgressBar = (progressBarContainer) => {
+  const progressBar = document.createElement("div")
+  progressBar.setAttribute("id", "progress-bar")
+  const myBar = document.createElement("div")
+  myBar.setAttribute("id", "my-bar")
+  progressBar.appendChild(myBar)
+  progressBarContainer.innerText = ""
+  progressBarContainer.appendChild(progressBar)
+  return myBar
+}
+
+const main = () => {
+  /** @type {HTMLDivElement | null} */
+  const progressBarContainer = document.querySelector("#progress-bar-container");
+
+  /** @type {HTMLInputElement | null} */
+  const startJobButton = document.querySelector("button[id=start-job]");
+
+  if(!!startJobButton && !!progressBarContainer) {
+    startJobButton.disabled = false;
+    startJobButton.onclick = () => {
+      startJobButton.disabled = true;
+      const myBar = setupProgressBar(progressBarContainer)
+      progress(myBar)
+    };
+  }
 };
 
 main();
