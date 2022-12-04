14f264f5f32237b529b46a0628ed1f29bfbabf9f
Michael Milewski <saramic@gmail.com>
2022-12-04 08:50:37 +0700

    Prepare day04 template for AoC

---
+++ AdventOfCode/2022/lib/day_04_program.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "input_helpers"
+class Day04Program
+  include InputHelpers
+
+  def perform(input_filename)
+    nil
+  end
+
+  def perform_pII(input_filename)
+    nil
+  end
+end
