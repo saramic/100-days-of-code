f1bff50816119f44daca67815febb0cba0f59c2b
Michael Milewski <saramic@gmail.com>
2022-12-06 12:35:07 +0700

    Attempt a small refactoring on day 06

--- AdventOfCode/2022/lib/day_06_tuning_trouble.rb
+++ AdventOfCode/2022/lib/day_06_tuning_trouble.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-require "input_helpers"
 class Day06TuningTrouble
-  include InputHelpers
-
   def perform(input_filename)
     calculate(input_filename, 4)
   end
@@ -14,8 +11,8 @@ class Day06TuningTrouble
 
   def calculate(filename, num)
     chars = File.read(filename).chars
-    (0..chars.size).each do |index|
-      return index + num if chars.slice(index, num).uniq.count == num
+    (num..chars.size).each do |index|
+      return index if chars.slice(index - num, num).uniq.count == num
     end
   end
 end
