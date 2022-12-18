04e964eb9c7382c6b8fe55996e63af9d76ecec89
Michael Milewski <saramic@gmail.com>
2022-12-18 14:43:58 +0700

    Setup 2FA for devise
    using:
      bundle add devise-two-factor
      bin/rails generate devise_two_factor User
      bin/rails db:migrate
      bin/rails db:encryption:init
      bin/rails credentials:edit
      rails generate devise:views
    
    also force add the config master.key for this play repo
      git add -f config/master.key
    
    can now sign up/login AND ask to generate an otp_secret
    
    in the db
      psql -d rails_2fa_play_development \
        -c "SELECT email, otp_secret FROM users;"
    
    in rails
      bin/rails runner 'pp User.all.map{|u| [u.email, u.otp_secret] }'
    
    and generate an OTP
      bin/rails runner "pp ROTP::TOTP.new(
        User.find_by(email: 'm@m.m').otp_secret).now"

--- 2fa_play/rails_2fa_play/app/controllers/home_controller.rb
+++ 2fa_play/rails_2fa_play/app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  def setup_2fa
+    current_user.otp_required_for_login = true
+    current_user.otp_secret = User.generate_otp_secret
+    current_user.save!
+    @otp_secret = current_user.otp_secret
+    render action: :index
+  end
 end
--- 2fa_play/rails_2fa_play/config/routes.rb
+++ 2fa_play/rails_2fa_play/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  resource :home, only: %I[index]
+  patch '/home/setup_2fa' => 'home#setup_2fa', as: :setup_2fa_home
   root "home#index"
 end
